f32076ac9344968a22f64bb9632b78504534e047
Fabio Santangelo <fabbomaui04gmail.com>
2024-11-17 16:11:56 +0100

    Completamento esercizio 3 javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+let currentYear = 2024; 
+
+console.log("Siamo nell'anno " + currentYear);
+
+let userAge = 20;
+
+console.log("L'user ha " + userAge + " anni");
+
+let userBirth = currentYear - userAge;
+
+console.log("L'user è nato nell'anno " + userBirth);
+
+let ageDifference = 100 - userAge;
+
+console.log("dato che hai " + userAge + " anni, ti mancano " + ageDifference + " anni per compierne 100")
+
